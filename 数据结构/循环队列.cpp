a02feba78238de3159391bf8ed09a9c72e7933ef
YifePlayte <yife-playte@foxmail.com>
2021-10-18 03:05:44 +0800

    完善循环队列main

--- 数据结构/循环队列.cpp
+++ 数据结构/循环队列.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXQSIZE = 100;
+const int MAXQSIZE = 7;
 
-typedef struct
+typedef struct SqQueue
 {
     int *base;
     int front;
@@ -72,12 +72,84 @@ void SqQPrint(SqQueue q)
     else
     {
         printf("%d", *(q.base + i));
-        i = (i + 1) % MAXSIZE;
+        i = (i + 1) % MAXQSIZE;
     }
     while (i != q.rear)
     {
         printf(" %d", *(q.base + i));
-        i = (i + 1) % MAXSIZE;
+        i = (i + 1) % MAXQSIZE;
     }
     cout << endl;
+}
+
+int main()
+{
+    SqQueue Q;
+    SqQInit(Q);
+    int n, d;
+    cout << "请输入队列长度：" << endl;
+    cin >> n;
+    cout << "请输入队列：" << endl;
+    while(n)
+    {
+        cin >> d;
+        SqQEn(Q, d);
+        n--;
+    }
+    cout << "队列内容为：" << endl;
+    SqQPrint(Q);
+    if (SqQHead(Q, d))
+    {
+        cout << "队头为：" << d << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    if (SqQDe(Q, d))
+    {
+        cout << "队头为：" << d << "，已被弹出" << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    if (SqQDe(Q, d))
+    {
+        cout << "队头为：" << d << "，已被弹出" << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    cout << "队列内容为：" << endl;
+    SqQPrint(Q);
+    cout << "队列长度为：" << SqQLength(Q) << endl;
+    cout << "请输入队列长度：" << endl;
+    cin >> n;
+    cout << "请输入队列：" << endl;
+    while(n)
+    {
+        cin >> d;
+        SqQEn(Q, d);
+        n--;
+    }
+    cout << "队列内容为：" << endl;
+    SqQPrint(Q);
+    if (SqQHead(Q, d))
+    {
+        cout << "队头为：" << d << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    if (SqQDe(Q, d))
+    {
+        cout << "队头为：" << d << "，已被弹出" << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    if (SqQDe(Q, d))
+    {
+        cout << "队头为：" << d << "，已被弹出" << endl;
+    }
+    else
+        cout << "队列为空！" << endl;
+    cout << "队列内容为：" << endl;
+    SqQPrint(Q);
+    cout << "队列长度为：" << SqQLength(Q) << endl;
+    return 0;
 }
